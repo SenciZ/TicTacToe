f868f7bcb19faf0e50ecb05d62edefeebacd31ba
SenciZ <zenkovic88@gmail.com>
2021-05-07 14:01:33 -0500

    Create starting gameboard

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("works")
+const gameBoard = (function () {
+  let gameArray = ["", "", "", "", "", "", "", "", ""];
+  let gameboard = document.querySelector(".gameboard");
+
+  gameArray.forEach(function (index) {
+    let field = document.createElement("div");
+    field.classList.add("field");
+    field.textContent = (`${gameArray[1]}`)
+    gameboard.appendChild(field);
+  });
+})();
+
+const Player = (player, x_or_o) => {
+  return { player, x_or_o };
+};
